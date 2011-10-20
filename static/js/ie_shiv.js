5c950bd7c710705c905bf7f0d0031647f7371e79
Brad Abrahams <brad@playfire.com>
2011-10-20 15:08:16 +0100

    initial commit - Flask with base template and IE JS shiv

---
+++ static/js/ie_shiv.js
@@ -0,0 +1,10 @@
+document.createElement("article");
+document.createElement("aside");
+document.createElement("figure");
+document.createElement("footer");
+document.createElement("header");
+document.createElement("hgroup");
+document.createElement("mark");
+document.createElement("nav");
+document.createElement("output");
+document.createElement("section");
