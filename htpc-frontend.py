1d10a2f3e5c2a48f46ad8f974538fbc5bb88573f
Brad Abrahams <brad@playfire.com>
2011-10-21 16:10:53 +0100

    only temporary as not having full episode info isn't really good enough, but make the filename a bit easier/nicer to read

--- htpc-frontend.py
+++ htpc-frontend.py
@@ -9,8 +9,13 @@ app = Flask(__name__)
 def index():
     xbmc = jsonrpclib.Server(SERVER_ADDRESS)
     episodes = xbmc.VideoLibrary.GetRecentlyAddedEpisodes()
+    recently_added_episodes = []
 
-    return render_template('index.html', recently_added_episodes = episodes)
+    for episode in episodes['episodes'][:NUM_RECENT_EPISODES]:
+        filename = episode['file'].split('/').pop().replace('.', ' ')
+        recently_added_episodes.append(filename)
+
+    return render_template('index.html', recently_added_episodes = recently_added_episodes)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- settings_example.py
+++ settings_example.py
@@ -1,1 +1,2 @@
 SERVER_ADDRESS = 'http://user:pass@server:port/jsonrpc'
+NUM_RECENT_EPISODES = 10
