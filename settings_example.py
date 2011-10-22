dc4536bd5c9dcfef0b4519023b6d0c2c09a5ca51
Brad Abrahams <email@bradabrahams.co.uk>
2011-10-22 12:31:42 +0100

    updated settings_example with description

--- settings_example.py
+++ settings_example.py
@@ -2,6 +2,14 @@ SERVER_ADDRESS = 'http://user:pass@server:port/jsonrpc'
 NUM_RECENT_EPISODES = 10
 
 APPLICATIONS = [
-    { 'name': 'XBMC', 'url': 'http://server:port/' },
-    { 'name': 'SABnzbd+', 'url': 'http://server:port/' },
+    {
+        'name': 'XBMC',
+        'url': 'http://server:port/',
+        'description': 'Library view'
+    },
+    {
+        'name': 'SABnzbd+',
+        'url': 'http://server:port/',
+        'description': 'Usenet binary client'
+    },
 ]
