a757cd1772cc212df19cd4e52768d3827c5d8558
Brad Abrahams <brad@playfire.com>
2011-10-24 12:15:35 +0100

    currently playing module (loaded in, not styled or wired up)

--- static/js/index.js
+++ static/js/index.js
@@ -6,4 +6,30 @@ $(document).ready(function() {
     $('#recently_added').fadeIn(200);
   });
 
+  function get_currently_playing() {
+    $.get('/xhr/currently_playing', function(data) {
+
+      if (data.playing === false) {
+        $('#currently_playing').fadeOut(200, function() {
+          $(this).remove();
+        });
+
+      } else {
+        var currently_playing_module = $('#currently_playing');
+
+        if (currently_playing_module.length > 0) {
+          currently_playing_module.replaceWith(data);
+
+        } else {
+          var module = $(data).hide();
+          $('body').append(module);
+          $('#currently_playing').slideDown(200);
+        }
+      }
+    });
+    setTimeout(get_currently_playing, 10000);
+  }
+
+  get_currently_playing();
+
 });
