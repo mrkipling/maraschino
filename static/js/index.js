1082f4fbd64384ac14905f1205c0e19a1cef1bc8
Brad Abrahams <brad@playfire.com>
2011-10-24 12:00:14 +0100

    subtle fade-in of module; looks kinda nice

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
   $.get('/xhr/recently_added', function(data) {
-    $('#recently_added_placeholder').replaceWith(data);
+    var module = $(data).hide();
+    $('#recently_added_placeholder').replaceWith(module);
+    $('#recently_added').fadeIn(200);
   });
 
 });
