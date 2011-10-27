d6972434d74b6cefa352942a4253f040118f95d4
Brad Abrahams <brad@playfire.com>
2011-10-27 16:37:55 +0100

    added comment to settings re. auth forwarding

--- settings_example.py
+++ settings_example.py
@@ -45,6 +45,8 @@ APPLICATIONS = [
 ]
 
 # include AUTH if you want to use HTTP basic authentication
+# if serving using Apache and mod_wsgi, make sure to enable auth forwarding:
+# http://code.google.com/p/modwsgi/wiki/ConfigurationDirectives#WSGIPassAuthorization
 
 #AUTH = {
 #    'username': 'username',
