8d269c755472cc882ff7489268155a7c717aa17c
Brad Abrahams <email@bradabrahams.co.uk>
2011-11-03 13:36:59 +0000

    moved controls into own file

--- htpcfrontend.py
+++ htpcfrontend.py
@@ -12,6 +12,7 @@ from recently_added import *
 from sabnzbd import *
 from trakt import *
 from currently_playing import *
+from controls import *
 
 @app.route('/')
 @requires_auth
@@ -22,47 +23,6 @@ def index():
         fanart_backgrounds = FANART_BACKGROUNDS,
         applications = APPLICATIONS,
     )
-
-@app.route('/xhr/play_episode/<int:episode_id>')
-@requires_auth
-def xhr_play_episode(episode_id):
-    xbmc = jsonrpclib.Server(SERVER_API_ADDRESS)
-    xbmc.Playlist.Clear(playlistid=1)
-
-    item = { 'episodeid': episode_id }
-    xbmc.Playlist.Add(playlistid=1, item=item)
-
-    item = { 'playlistid': 1 }
-    xbmc.Player.Open(item)
-
-    return jsonify({ 'success': True })
-
-@app.route('/xhr/play_movie/<int:movie_id>')
-@requires_auth
-def xhr_play_movie(movie_id):
-    xbmc = jsonrpclib.Server(SERVER_API_ADDRESS)
-    xbmc.Playlist.Clear(playlistid=1)
-
-    item = { 'movieid': movie_id }
-    xbmc.Playlist.Add(playlistid=1, item=item)
-
-    item = { 'playlistid': 1 }
-    xbmc.Player.Open(item)
-
-    return jsonify({ 'success': True })
-
-@app.route('/xhr/controls/<command>')
-@requires_auth
-def xhr_controls(command):
-    xbmc = jsonrpclib.Server(SERVER_API_ADDRESS)
-
-    if command == 'play_pause':
-        xbmc.Player.PlayPause(playerid=1)
-
-    elif command == 'stop':
-        xbmc.Player.Stop(playerid=1)
-
-    return jsonify({ 'success': True })
 
 @app.route('/xhr/library')
 @requires_auth
