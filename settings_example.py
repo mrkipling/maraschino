a21cb60ee7bb93edade68918dd983d5a7415e183
Brad Abrahams <email@bradabrahams.co.uk>
2011-11-05 14:16:29 +0000

    got rid of unneeded stuff from settings_example.py

--- settings_example.py
+++ settings_example.py
@@ -1,18 +1,3 @@
-MODULES = [
-    [
-        { 'module': 'applications', 'static': True },
-        { 'module': 'library' },
-    ],
-    [
-        { 'module': 'synopsis' },
-        { 'module': 'trakt' },
-    ],
-    [
-        { 'module': 'recently_added', 'poll': 350 },
-        { 'module': 'sabnzbd', 'poll': 10 },
-    ],
-]
-
 DATABASE = '/path/to/maraschino.db'
 
 SHOW_CURRENTLY_PLAYING = True
@@ -25,20 +10,6 @@ SERVER = {
     'password': ''
 }
 
-# required for module 'sabnzbd'
-SABNZBD_URL = 'http://server:port/api?apikey=b72734e6901ef6d69659759974440f21'
-
-# required for module 'trakt'
-TRAKT_API_KEY = None
-
-# required to enable shout posting for module 'trakt'
-TRAKT_USERNAME = None
-TRAKT_PASSWORD = None
-
-# required for module 'recently_added'
-NUM_RECENT_EPISODES = 5
-
-# required for module 'applications'
 APPLICATIONS = [
     {
         'name': 'XBMC',
