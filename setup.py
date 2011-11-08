d8dffb51fdcc70fba227b7a91a229885141c6146
Brad Abrahams <email@bradabrahams.co.uk>
2011-11-08 10:39:49 +0000

    more descriptive error message in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ try:
     from database import *
 
 except:
-    print "You need to specify DATABASE in settings.py"
+    print "You need to specify DATABASE in settings.py, and ensure that Flask-SQLAlchemy is installed."
     quit()
 
 init_db()
