b65c942a266bc733723f8c765e52266cf1961076
Brad Abrahams <email@bradabrahams.co.uk>
2011-11-09 13:52:49 +0000

    made recently added a bit more generic so that we're not copy-pasting to make a recently added movies module

--- recently_added.py
+++ recently_added.py
@@ -9,33 +9,55 @@ from tools import *
 @app.route('/xhr/recently_added')
 @requires_auth
 def xhr_recently_added():
-    return render_recently_added_episodes()
+    return render_recently_added(
+        itemtype = 'episodes'
+    )
 
 @app.route('/xhr/recently_added/<int:offset>')
 @requires_auth
 def xhr_recently_added_offset(offset):
-    return render_recently_added_episodes(offset)
+    return render_recently_added(
+        itemtype = 'episodes',
+        offset = offset,
+    )
 
-def render_recently_added_episodes(offset=0):
+@app.route('/xhr/recently_added_movies')
+@requires_auth
+def xhr_recently_added_movies():
+    return render_recently_added(
+        itemtype = 'movies'
+    )
+
+@app.route('/xhr/recently_added_movies/<int:offset>')
+@requires_auth
+def xhr_recently_added_movies_offset(offset):
+    return render_recently_added_movies(
+        itemtype = 'movies',
+        offset = offset,
+    )
+
+def render_recently_added(itemtype='episodes', offset=0):
+    xbmc = jsonrpclib.Server(server_api_address())
+
     try:
-        xbmc = jsonrpclib.Server(server_api_address())
-        recently_added_episodes = xbmc.VideoLibrary.GetRecentlyAddedEpisodes(properties = ['title', 'season', 'episode', 'showtitle', 'lastplayed', 'thumbnail'])
-        vfs_url = '%s/vfs/' % (safe_server_address())
+        if itemtype == 'episodes':
+            recently_added = xbmc.VideoLibrary.GetRecentlyAddedEpisodes(properties = ['title', 'season', 'episode', 'showtitle', 'lastplayed', 'thumbnail'])
+            vfs_url = '%s/vfs/' % (safe_server_address())
 
-        try:
-            NUM_RECENT_EPISODES = int(get_setting('num_recent_episodes').value)
+            try:
+                NUM_RECENT_EPISODES = int(get_setting('num_recent_episodes').value)
 
-        except:
-            NUM_RECENT_EPISODES = 5
+            except:
+                NUM_RECENT_EPISODES = 5
 
-        recently_added_episodes = recently_added_episodes['episodes'][offset:NUM_RECENT_EPISODES + offset]
+            recently_added = recently_added['episodes'][offset:NUM_RECENT_EPISODES + offset]
 
     except:
-        recently_added_episodes = []
+        recently_added = []
         vfs_url = None
 
     return render_template('recently_added.html',
-        recently_added_episodes = recently_added_episodes,
+        recently_added = recently_added,
         vfs_url = vfs_url,
         offset = offset,
     )
