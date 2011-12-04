0f0995ccfcb23fc89fda14e8e25b5f34c0f7c4e2
DejaVu <deja-vu@webname.com>
2011-12-04 20:28:42 +0000

    More SabNZBd+ Tipsy's and Adjust Speed looks better

--- static/js/lib/tipsy.js
+++ static/js/lib/tipsy.js
@@ -1,6 +1,8 @@
   $(function() {
   $('li').tipsy({gravity:'s'});
-  $('a').tipsy({gravity:'s'});  
+  $('a').tipsy({gravity:'s'});
   $('div').tipsy({gravity:'s'});
-  $('strong').tipsy({gravity:'s'});  
+  $('strong').tipsy({gravity:'s'});
+  $('input').tipsy({gravity:'s'});
+  $('img').tipsy({gravity:'s'});    
   });
