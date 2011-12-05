38c135819318db06a7b664d64b2d9f4115b7b23c
gugahoi <guga@maurorisch.com.br>
2011-12-05 13:49:08 +1100

    Removed Tipsy from every module

--- static/js/lib/tipsy.js
+++
@@ -1,8 +0,0 @@
-  $(function() {
-  $('li').tipsy({gravity:'s'});
-  $('a').tipsy({gravity:'s'});
-  $('div').tipsy({gravity:'s'});
-  $('strong').tipsy({gravity:'s'});
-  $('input').tipsy({gravity:'s'});
-  $('img').tipsy({gravity:'s'});
-  });
