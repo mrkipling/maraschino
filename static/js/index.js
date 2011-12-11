71c317e50677152b6aa5a37330befcb577d49e95
Brad Abrahams <email@bradabrahams.co.uk>
2011-12-11 14:14:52 +0000

    commented out tipsy for now

--- static/js/index.js
+++ static/js/index.js
@@ -956,7 +956,8 @@ $(document).ready(function() {
       }
     });
   });
-  
+
+  /*
   //Tipsy Initialisation
   $('li').tipsy({gravity:'s', live: true, fade:true});
   $('a').tipsy({gravity:'s', live: true, fade:true});
@@ -964,6 +965,7 @@ $(document).ready(function() {
   $('strong').tipsy({gravity:'s', live: true, fade:true});
   $('input').tipsy({gravity:'s', live: true, fade:true});
   $('img').tipsy({gravity:'s', live: true, fade:true});
-  $('.status').tipsy({gravity:'s', live: true, fade:true});  
+  $('.status').tipsy({gravity:'s', live: true, fade:true});
+  */
 
 });
