b68c48fca2bee628b3d83d19c952e123880dfbed
Brad Abrahams <email@bradabrahams.co.uk>
2011-12-17 14:28:00 +0000

    fixed issue where Sickbeard items were jumping around by 1px on hover

--- static/js/index.js
+++ static/js/index.js
@@ -488,7 +488,7 @@ $(document).ready(function() {
 
   $('#sickbeard .coming_ep').live('hover', function(){
     var id = ($(this).attr('id'));
-    $('#sickbeard #'+id+' .details').toggle(200);
+    $('#sickbeard #'+id+' .details').fadeToggle(200);
   });
 
   // Plot display function
@@ -546,7 +546,7 @@ $(document).ready(function() {
   // Show Menu
 
   $('body').delegate('#sickbeard .menu-icon', 'click', function(){
-    $('#sickbeard .menu').toggle(200);
+    $('#sickbeard .menu').fadeToggle(200);
   });
 
   // Show info
