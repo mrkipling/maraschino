904e6300af333fae00e9eb221f2e506352066070
Mar2zz <lasi.mar2zz@gmail.com>
2011-12-29 19:51:14 +0100

    create a database @ first launch

--- setup.py
+++
@@ -1,9 +0,0 @@
-try:
-    from database import *
-
-except:
-    print "You need to specify DATABASE in settings.py, and ensure that Flask-SQLAlchemy is installed."
-    quit()
-
-init_db()
-print "Database successfully initialised."
