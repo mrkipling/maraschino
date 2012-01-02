65646a9c03f5d9a7baf4f7aba0e6886de3109d20
Mar2zz <lasi.mar2zz@gmail.com>
2012-01-02 21:42:32 +0100

    now used mv. tested and works for me. also added a warning to create a settings.py if it is not found.

---
+++ Maraschino.py
@@ -0,0 +1,128 @@
+import sys
+import os
+
+rundir = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    frozen = sys.frozen
+except AttributeError:
+    frozen = False
+
+# Define path based on frozen state
+if frozen:
+    path_base = os.environ['_MEIPASS2']
+    rundir = os.path.dirname(sys.executable)
+
+else:
+    path_base = rundir
+
+# Include paths
+sys.path.insert(0, path_base)
+sys.path.insert(0, os.path.join(path_base, 'lib'))
+
+from flask import Flask, jsonify, render_template, request
+from maraschino.database import db_session
+import hashlib, json, jsonrpclib, random, urllib, os, sys
+
+from flask import Flask, jsonify, render_template, request
+from maraschino.database import db_session
+import hashlib, json, jsonrpclib, random, urllib, os, sys
+
+app = Flask(__name__)
+
+from settings import *
+
+from maraschino.noneditable import *
+from maraschino.tools import *
+
+from modules.applications import *
+from modules.controls import *
+from modules.currently_playing import *
+from modules.diskspace import *
+from modules.library import *
+from modules.recently_added import *
+from modules.recommendations import *
+from modules.sabnzbd import *
+from modules.sickbeard import *
+from modules.trakt import *
+
+from maraschino.modules import *
+from maraschino.models import Module, Setting
+
+@app.route('/')
+@requires_auth
+def index():
+    unorganised_modules = Module.query.order_by(Module.position)
+    modules = [[],[],[]]
+
+    for module in unorganised_modules:
+        module_info = get_module_info(module.name)
+        module.template = '%s.html' % (module.name)
+        module.static = module_info['static']
+        modules[module.column - 1].append(module)
+
+    applications = []
+
+    try:
+        applications = Application.query.order_by(Application.position)
+
+    except:
+        pass
+
+    # select random background when not watching media
+
+    background = None
+
+    if get_setting_value('random_backgrounds') == '1':
+        try:
+            backgrounds = []
+            backgrounds.extend(get_file_list('static/images/backgrounds/', ['.jpg', '.png']))
+            background = backgrounds[random.randrange(0, len(backgrounds))]
+
+        except:
+            background = None
+
+    # show fanart backgrounds when watching media
+    fanart_backgrounds = get_setting_value('fanart_backgrounds') == '1'
+
+    return render_template('index.html',
+        modules = modules,
+        show_currently_playing = True,
+        background = background,
+        fanart_backgrounds = fanart_backgrounds,
+        applications = applications,
+        show_tutorial = unorganised_modules.count() == 0,
+    )
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+
+# check if database exists or create it
+
+try:
+    open(DATABASE)
+
+except IOError as e:
+    try:
+        # check if path exists
+        dbpath = os.path.dirname(DATABASE)
+        if not os.path.exists(dbpath):
+            try:
+                os.makedirs(dbpath)
+            except:
+               print 'Could not create %s, check settings.py.'% (DATABASE)
+               quit()
+
+        # create db
+        from maraschino.database import *
+
+    except:
+        print 'You need to specify a database in settings.py.'
+        quit()
+
+    init_db()
+    print "Database successfully initialised."
+
+if __name__ == '__main__':
+    app.run(debug=True, port=PORT, host='0.0.0.0')
---
+++ modules/sickbeard.py
@@ -0,0 +1,349 @@
+from flask import Flask, jsonify, render_template, request, send_file
+import json, jsonrpclib, urllib
+
+from Maraschino import app
+from settings import *
+from maraschino.tools import *
+
+def login_string():
+    try:
+        login = '%s:%s@' % (get_setting('sickbeard_user').value, get_setting('sickbeard_password').value)
+
+    except:
+        login = ''
+
+    return login
+
+def sickbeard_url():
+    url = '%s:%s/api/%s' % (get_setting_value('sickbeard_ip'), get_setting_value('sickbeard_port'), get_setting_value('sickbeard_api'))
+
+    if using_auth():
+        return 'http://%s%s' % (login_string(), url)
+
+    return 'http://%s' % (url)
+
+def sickbeard_url_no_api():
+    url = '%s:%s/' % (get_setting_value('sickbeard_ip'), get_setting_value('sickbeard_port'))
+
+    if using_auth():
+        return 'http://%s%s' % (login_string(), url)
+
+    return 'http://%s' % (url)
+
+@app.route('/xhr/sickbeard')
+def xhr_sickbeard():
+    try:
+        url = '%s/?cmd=future&sort=date' % (sickbeard_url())
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+        compact_view = get_setting_value('sickbeard_compact') == '1'
+
+    except:
+        raise Exception
+
+    if sickbeard['result'].rfind('success') >= 0:
+        sickbeard = sickbeard['data']
+
+    else:
+        sickbeard = ''
+
+    return render_template('sickbeard.html',
+        url = sickbeard_url_no_api(),
+        sickbeard = sickbeard,
+        missed = sickbeard['missed'],
+        today = sickbeard['today'],
+        soon = sickbeard['soon'],
+        later = sickbeard['later'],
+        compact_view = compact_view,
+    )
+
+@app.route('/sickbeard/search_ep/<tvdbid>/<season>/<episode>')
+@requires_auth
+def search_ep(tvdbid, season, episode):
+    try:
+        url = '%s/?cmd=episode.search&tvdbid=%s&season=%s&episode=%s' %(sickbeard_url(), tvdbid, season, episode)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    if sickbeard['result'].rfind('success') >= 0:
+        return sickbeard
+
+    return ''
+
+@app.route('/sickbeard/get_plot/<tvdbid>/<season>/<episode>')
+def get_plot(tvdbid, season, episode):
+    try:
+        url = '%s/home/plotDetails?show=%s&episode=%s&season=%s' %(sickbeard_url_no_api(), tvdbid, episode, season)
+        plot = urllib.urlopen(url).read()
+
+    except:
+        raise Exception
+
+    if plot:
+        return plot
+
+    return ''
+
+@app.route('/sickbeard/get_all')
+def get_all():
+    try:
+        url = '%s/?cmd=shows&sort=name' %(sickbeard_url())
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    if sickbeard['result'].rfind('success') >=0:
+        sickbeard = sickbeard['data']
+
+        for show in sickbeard:
+            sickbeard[show]['url'] = get_pic(sickbeard[show]['tvdbid'], 'banner')
+
+    return render_template('sickbeard-all.html',
+        sickbeard = sickbeard,
+    )
+
+@app.route('/sickbeard/get_show_info/<tvdbid>')
+def show_info(tvdbid):
+    try:
+        url = '%s/?cmd=show&tvdbid=%s' %(sickbeard_url(), tvdbid)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    if sickbeard['result'].rfind('success') >= 0:
+        sickbeard = sickbeard['data']
+        sickbeard['url'] = get_pic(tvdbid, 'banner')
+        sickbeard['tvdb'] = tvdbid
+
+    return render_template('sickbeard-show.html',
+        sickbeard = sickbeard,
+    )
+
+@app.route('/sickbeard/get_season/<tvdbid>/<season>')
+def get_season(tvdbid, season):
+    try:
+        url = '%s/?cmd=show.seasons&tvdbid=%s&season=%s' %(sickbeard_url(), tvdbid, season)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    if sickbeard['result'].rfind('success') >= 0:
+        sickbeard = sickbeard['data']
+
+    return render_template('sickbeard-season.html',
+       sickbeard = sickbeard,
+        id = tvdbid,
+        season = season,
+    )
+
+@app.route('/sickbeard/history/<limit>')
+def history(limit):
+    try:
+        url = '%s/?cmd=history&limit=%s' %(sickbeard_url(), limit)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    if sickbeard['result'].rfind('success') >= 0:
+        sickbeard = sickbeard['data']
+
+        for show in sickbeard:
+            show['image'] = get_pic(show['tvdbid'])
+
+    return render_template('sickbeard-history.html',
+        sickbeard = sickbeard,
+    )
+
+# returns a link with the path to the required image from SB
+def get_pic(tvdb, style='banner'):
+    url = '%s:%s' %(get_setting_value('sickbeard_ip'), get_setting_value('sickbeard_port'))
+    return 'http://%s/showPoster/?show=%s&which=%s' %(url, tvdb, style)
+
+@app.route('/sickbeard/get_ep_info/<tvdbid>/<season>/<ep>')
+def get_episode_info(tvdbid, season, ep):
+    try:
+        url = '%s/?cmd=episode&tvdbid=%s&season=%s&episode=%s&full_path=1' %(sickbeard_url(), tvdbid, season, ep)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    if sickbeard['result'].rfind('success') >= 0:
+        sickbeard = sickbeard['data']
+
+    return render_template('sickbeard-episode.html',
+        sickbeard = sickbeard,
+        id = tvdbid,
+        season = season,
+        ep = ep,
+    )
+
+@app.route('/sickbeard/set_ep_status/<tvdbid>/<season>/<ep>/<st>')
+def set_episode_status(tvdbid, season, ep, st):
+    try:
+        url = '%s/?cmd=episode.setstatus&tvdbid=%s&season=%s&episode=%s&status=%s' %(sickbeard_url(), tvdbid, season, ep, st)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    status = 'error'
+
+    if sickbeard['result'] == 'success':
+        status = 'success'
+
+    return jsonify({ 'status': status })
+
+@app.route('/sickbeard/shutdown')
+def shutdown():
+    try:
+        url = '%s/?cmd=sb.shutdown' %(sickbeard_url())
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    return sickbeard['message']
+
+@app.route('/sickbeard/restart')
+def restart():
+    try:
+        url = '%s/?cmd=sb.restart' %(sickbeard_url())
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    return sickbeard['message']
+
+@app.route('/sickbeard/search/')
+def search():
+    sickbeard = {}
+    params = ''
+
+    try:
+        params = '&name=%s' % (request.args['name'])
+    except:
+        pass
+
+    try:
+        params = '&tvdbid=%s' % (request.args['tvdbid'])
+    except:
+        pass
+
+    try:
+        params = '&lang=%s' % (request.args['lang'])
+    except:
+        pass
+
+    if params is not '':
+        try:
+            url = '%s/?cmd=sb.searchtvdb%s' %(sickbeard_url(), params)
+            result = urllib.urlopen(url).read()
+            sickbeard = json.JSONDecoder().decode(result)
+            sickbeard = sickbeard['data']['results']
+
+        except:
+            sickbeard = None
+
+    else:
+        sickbeard = None
+
+    return render_template('sickbeard-search.html',
+        data = sickbeard,
+        sickbeard = 'results',
+    )
+
+@app.route('/sickbeard/add_show/<tvdbid>')
+def add_show(tvdbid):
+    try:
+        url = '%s/?cmd=show.addnew&tvdbid=%s' %(sickbeard_url(), tvdbid)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    return sickbeard['message']
+
+@app.route('/sickbeard/get_banner/<tvdbid>')
+def get_banner(tvdbid):
+    import StringIO
+    url = '%s/?cmd=show.getbanner&tvdbid=%s' %(sickbeard_url(), tvdbid)
+    img = StringIO.StringIO(urllib.urlopen(url).read())
+    return send_file(img, mimetype='image/jpeg')
+
+@app.route('/sickbeard/get_poster/<tvdbid>')
+def get_poster(tvdbid):
+    import StringIO
+    url = '%s/?cmd=show.getposter&tvdbid=%s' %(sickbeard_url(), tvdbid)
+    img = StringIO.StringIO(urllib.urlopen(url).read())
+    return send_file(img, mimetype='image/jpeg')
+    
+@app.route('/sickbeard/log/<level>')
+def log(level):
+    try:
+        url = '%s/?cmd=logs&min_level=%s' %(sickbeard_url(), level)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+        if sickbeard['result'].rfind('success') >= 0:
+            sickbeard = sickbeard['data']
+    except:
+        sickbeard = None
+
+    return render_template('sickbeard-log.html',
+        sickbeard = sickbeard,
+        level = level,
+    )
+    
+@app.route('/sickbeard/delete_show/<tvdbid>')
+def delete_show(tvdbid):
+    try:
+        url = '%s/?cmd=show.delete&tvdbid=%s' %(sickbeard_url(), tvdbid)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    return sickbeard['message']
+
+@app.route('/sickbeard/refresh_show/<tvdbid>')
+def refresh_show(tvdbid):
+    try:
+        url = '%s/?cmd=show.refresh&tvdbid=%s' %(sickbeard_url(), tvdbid)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    return sickbeard['message']
+
+@app.route('/sickbeard/update_show/<tvdbid>')
+def update_show(tvdbid):
+    try:
+        url = '%s/?cmd=show.update&tvdbid=%s' %(sickbeard_url(), tvdbid)
+        result = urllib.urlopen(url).read()
+        sickbeard = json.JSONDecoder().decode(result)
+
+    except:
+        raise Exception
+
+    return sickbeard['message']
