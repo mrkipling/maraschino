3778523917d642e672c51388db90f7dbd821a0e7
gugahoi <guga@maurorisch.com.br>
2012-01-05 16:06:48 +1100

    Added compact view to recently added modules. Also fixed poster width on movies module.

--- recently_added.py
+++ recently_added.py
@@ -31,6 +31,7 @@ def xhr_recently_added_movies_offset(movie_offset):
 
 
 def render_recently_added_episodes(episode_offset=0):
+    compact_view = get_setting_value('server_compact') == '1'
     try:
         xbmc = jsonrpclib.Server(server_api_address())
         recently_added_episodes = get_recently_added_episodes(xbmc, episode_offset)
@@ -44,10 +45,12 @@ def render_recently_added_episodes(episode_offset=0):
         recently_added_episodes = recently_added_episodes,
         vfs_url = vfs_url,
         episode_offset = episode_offset,
+        compact_view = compact_view,
     )
 
 
 def render_recently_added_movies(movie_offset=0):
+    compact_view = get_setting_value('server_compact') == '1'
     try:
         xbmc = jsonrpclib.Server(server_api_address())
         recently_added_movies = get_recently_added_movies(xbmc, movie_offset)
@@ -61,6 +64,7 @@ def render_recently_added_movies(movie_offset=0):
         recently_added_movies = recently_added_movies,
         vfs_url = vfs_url,
         movie_offset = movie_offset,
+        compact_view = compact_view,
     )
 
 
