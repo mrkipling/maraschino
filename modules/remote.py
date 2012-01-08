4bc9e912c858088165342ea0da07d461eafda08d
Brad Abrahams <email@bradabrahams.co.uk>
2012-01-08 12:19:16 +0000

    trailing whitespace

--- modules/remote.py
+++ modules/remote.py
@@ -19,51 +19,51 @@ def remote(key):
     if not connected:
         if xbmc.connect():
             connected = True
-            
+
     xbmc.send_keyboard_button(key)
 
     time.sleep(0.3)
-    
+
     if xbmc.release_button():
-            return jsonify({ 'status': 'successful'})    
+            return jsonify({ 'status': 'successful'})
 
     return jsonify({ 'error': 'failed'})
-    
+
 @app.route('/remote/ping')
 def ping():
     global connected
 
     xbmc = update_xbmc_object()
-    
+
     if xbmc.ping():
         connected = True
         return jsonify({ 'status': 'successful'})
-    
+
     connected = False
-    return jsonify({ 'error': 'failed'})    
+    return jsonify({ 'error': 'failed'})
 
 @app.route('/remote/close')
 def close():
     global connected
-    
+
     xbmc = update_xbmc_object()
-    
+
     if xbmc.close():
         connected = False
         return jsonify({ 'status': 'successful'})
-    
+
     return jsonify({ 'error': 'failed'})
 
 @app.route('/remote/connect')
 def connect():
     global connected
-    
+
     xbmc = update_xbmc_object()
 
     if xbmc.connect():
         connected = True
         return jsonify({ 'status': 'successful'})
-    
+
     return jsonify({ 'error': 'failed'})
 
 def update_xbmc_object():
