4c8177400ca4e947b4ecee4f0aa9df6ae984da6f
gugahoi <guga@maurorisch.com.br>
2012-02-05 20:33:42 +1100

    Added pause/resume sabnzbd function Split sabnzbd templates into parent/child structure for future expandability

--- modules/sabnzbd.py
+++ modules/sabnzbd.py
@@ -5,25 +5,25 @@ from Maraschino import app
 from settings import *
 from maraschino.tools import *
 
+def sabnzbd_url_no_api():
+    return 'http://%s:%s' % (get_setting_value('sabnzbd_host'), get_setting_value('sabnzbd_port'))
+
+def sabnzbd_url(mode, extra = ""):
+    return '%s/api?apikey=%s&mode=%s&output=json%s' % (sabnzbd_url_no_api(), get_setting_value('sabnzbd_api'), mode, extra)
+
 @app.route('/xhr/sabnzbd')
 @requires_auth
 def xhr_sabnzbd():
-    SABNZBD_HOST = get_setting_value('sabnzbd_host')
-    SABNZBD_PORT = get_setting_value('sabnzbd_port')
-    SABNZBD_API = get_setting_value('sabnzbd_api')
-
-    SABNZBD_URL = 'http://%s:%s' % (SABNZBD_HOST, SABNZBD_PORT)
-
     old_config = False
 
-    if not SABNZBD_HOST:
+    if not get_setting_value('sabnzbd_host'):
         if get_setting_value('sabnzbd_url') != None:
             old_config = True
 
     try:
-        url = '%s/api?apikey=%s&mode=qstatus&output=json' % (SABNZBD_URL, SABNZBD_API)
-        result = urllib.urlopen(url).read()
+        result = urllib.urlopen(sabnzbd_url('queue')).read()
         sabnzbd = json.JSONDecoder().decode(result)
+        sabnzbd = sabnzbd['queue']
 
         percentage_total = 0
         download_speed = '%s kB/s' % (int(sabnzbd['kbpersec']))
@@ -39,9 +39,31 @@ def xhr_sabnzbd():
         percentage_total = None
         download_speed = None
 
-    return render_template('sabnzbd.html',
+    return render_template('sabnzbd-queue.html',
         sabnzbd = sabnzbd,
         percentage_total = percentage_total,
         download_speed = download_speed,
         old_config = old_config,
     )
+
+@app.route('/xhr/sabnzbd/<action>/')
+@app.route('/xhr/sabnzbd/pause/<time>')
+@requires_auth
+def sabnzb_pause(action = "pause", time = None):
+    if not time:
+        try:
+            result = urllib.urlopen(sabnzbd_url(action)).read()
+            sabnzbd = json.JSONDecoder().decode(result)
+        except:
+            pass
+    else:
+        try:
+            result = urllib.urlopen(sabnzbd_url('config', '&name=set_pause&value='+time)).read()
+            sabnzbd = json.JSONDecoder().decode(result)
+        except:
+            pass
+    
+    if sabnzbd:
+        return jsonify ({'status': sabnzbd['status']})
+    
+    return jsonify ({'status': False})
