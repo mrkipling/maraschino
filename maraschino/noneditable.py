4f1732ce479721631fb75f3306ae2815b1dcc9ed
Brad Abrahams <email@bradabrahams.co.uk>
2012-04-14 18:18:34 +0100

    model for XbmcServer, active_server setting, and create XbmcServer from legacy settings if exists

--- maraschino/noneditable.py
+++ maraschino/noneditable.py
@@ -1,14 +1,56 @@
 from settings import *
 from maraschino.tools import *
 
-from maraschino.models import Module, Setting
+from maraschino.models import Module, Setting, XbmcServer
 
 def server_settings():
+    servers = XbmcServer.query.order_by(XbmcServer.position)
+
+    if servers.count() == 0:
+        # check if old server settings value is set
+        old_server_hostname = get_setting_value('server_hostname')
+
+        # create an XbmcServer entry using the legacy settings
+        if old_server_hostname:
+            xbmc_server = XbmcServer(
+                'XBMC server 1',
+                1,
+                old_server_hostname,
+                get_setting_value('server_port'),
+                get_setting_value('server_username'),
+                get_setting_value('server_password'),
+                get_setting_value('server_macaddress'),
+            )
+
+            try:
+                db_session.add(xbmc_server)
+                db_session.commit()
+                servers = XbmcServer.query.order_by(XbmcServer.position)
+
+            except:
+                logger.log('Could not create new XbmcServer based on legacy settings' , 'WARNING')
+
+    active_server = get_setting_value('active_server')
+
+    # if active server is not defined, set it
+
+    if not active_server:
+        active_server = Setting('active_server', servers.first().id)
+        db_session.add(active_server)
+        db_session.commit()
+
+    try:
+        server = servers.get(active_server)
+
+    except:
+        logger.log('Could not retrieve active server, falling back on first entry' , 'WARNING')
+        server = servers.first()
+
     return {
-        'hostname': get_setting_value('server_hostname'),
-        'port': get_setting_value('server_port'),
-        'username': get_setting_value('server_username'),
-        'password': get_setting_value('server_password'),
+        'hostname': server.hostname,
+        'port': server.port,
+        'username': server.username,
+        'password': server.password,
     }
 
 def server_username_password():
