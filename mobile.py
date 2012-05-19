aaf10d2e974e21a15a3d3383197ccd969f3ca446
Brad Abrahams <email@bradabrahams.co.uk>
2012-05-19 15:40:38 +0100

    changed URLs around a bit

--- mobile.py
+++ mobile.py
@@ -1,9 +1,19 @@
+import jsonrpclib
+import maraschino.logger as logger
+
 from flask import Flask, render_template
 from Maraschino import app
 
 from maraschino.tools import *
+from maraschino.noneditable import *
+from modules.recently_added import get_recently_added_episodes
+
+@app.route('/mobile/temp_index_url')
+@requires_auth
+def mobile_index():
+    return render_template('mobile/index.html')
 
 @app.route('/mobile')
 @requires_auth
-def mobile_index():
+def recently_added_episodes():
     return render_template('mobile/recent_episodes.html')
