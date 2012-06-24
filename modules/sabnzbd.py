85a7c52e5f4e83ad41649f14838e42dcda778b2a
Gustavo Hoirisch <guga@maurorisch.com.br>
2012-06-24 23:05:16 +1000

    Added total remaining time and size to sab queue

--- modules/sabnzbd.py
+++ modules/sabnzbd.py
@@ -10,11 +10,13 @@ from jinja2.filters import FILTERS
 from Maraschino import app
 from maraschino.tools import *
 
+
 def sab_http():
     if get_setting_value('sabnzbd_https') == '1':
         return 'https://'
     else:
         return 'http://'
+
 
 def sabnzbd_url_no_api():
     url_base = get_setting_value('sabnzbd_host')
@@ -25,8 +27,10 @@ def sabnzbd_url_no_api():
 
     return sab_http() + url_base
 
-def sabnzbd_url(mode, extra = ""):
+
+def sabnzbd_url(mode, extra=""):
     return '%s/api?apikey=%s&mode=%s&output=json%s' % (sabnzbd_url_no_api(), get_setting_value('sabnzbd_api'), mode, extra)
+
 
 def sab_link():
     SABNZBD_HOST = get_setting_value('sabnzbd_host')
@@ -37,6 +41,7 @@ def sab_link():
 
     return '%s/api?apikey=%s' % (SABNZBD_URL, SABNZBD_API)
 
+
 def add_to_sab_link(nzb):
     if get_setting_value('sabnzbd_api') != None:
         return '%s&mode=addurl&name=http://%s&output=json' % (sab_link(), nzb)
@@ -44,10 +49,11 @@ def add_to_sab_link(nzb):
 
 FILTERS['add_to_sab'] = add_to_sab_link
 
+
 @app.route('/xhr/sabnzbd/')
 @app.route('/xhr/sabnzbd/<queue_status>')
 @requires_auth
-def xhr_sabnzbd(queue_status = 'hide'):
+def xhr_sabnzbd(queue_status='hide'):
     old_config = False
 
     if not get_setting_value('sabnzbd_host'):
@@ -56,17 +62,15 @@ def xhr_sabnzbd(queue_status = 'hide'):
 
     downloading = None
     sabnzbd = None
-    percentage_total = None
     download_speed = None
     downloading = None
-    download_left = None
 
     try:
         result = urllib.urlopen(sabnzbd_url('queue')).read()
         sabnzbd = json.JSONDecoder().decode(result)
         sabnzbd = sabnzbd['queue']
 
-        download_speed = format_number(int((sabnzbd['kbpersec'])[:-3])*1024) + '/s'
+        download_speed = format_number(int((sabnzbd['kbpersec'])[:-3]) * 1024) + '/s'
 
         if sabnzbd['slots']:
             for item in sabnzbd['slots']:
@@ -74,23 +78,22 @@ def xhr_sabnzbd(queue_status = 'hide'):
                     downloading = item
                     break
 
-        download_left = format_number(int(float(sabnzbd['mbleft'])*1024*1024))
-
     except:
         pass
 
     return render_template('sabnzbd-queue.html',
-        sabnzbd = sabnzbd,
-        item = downloading,
-        download_speed = download_speed,
-        old_config = old_config,
-        queue_status = queue_status,
+        sabnzbd=sabnzbd,
+        item=downloading,
+        download_speed=download_speed,
+        old_config=old_config,
+        queue_status=queue_status,
     )
+
 
 @app.route('/xhr/sabnzbd/queue/<action>/')
 @app.route('/xhr/sabnzbd/queue/pause/<time>')
 @requires_auth
-def sabnzb_queue(action = "pause", time = None):
+def sabnzb_queue(action="pause", time=None):
     if not time:
         try:
             result = urllib.urlopen(sabnzbd_url(action)).read()
@@ -99,50 +102,53 @@ def sabnzb_queue(action = "pause", time = None):
             pass
     else:
         try:
-            result = urllib.urlopen(sabnzbd_url('config', '&name=set_pause&value='+time)).read()
+            result = urllib.urlopen(sabnzbd_url('config', '&name=set_pause&value=' + time)).read()
             sabnzbd = json.JSONDecoder().decode(result)
         except:
             pass
 
     if sabnzbd:
-        return jsonify ({'status': sabnzbd['status']})
+        return jsonify({'status': sabnzbd['status']})
 
-    return jsonify ({'status': False})
+    return jsonify({'status': False})
+
 
 @app.route('/xhr/sabnzbd/speedlimit/<int:speed>/')
 @requires_auth
 def sabnzb_speed_limit(speed):
     try:
-        result = urllib.urlopen(sabnzbd_url('config', '&name=speedlimit&value=%i'%(speed))).read()
+        result = urllib.urlopen(sabnzbd_url('config', '&name=speedlimit&value=%i' % (speed))).read()
         sabnzbd = json.JSONDecoder().decode(result)
         if sabnzbd:
-            return jsonify ({'status': sabnzbd['status']})
+            return jsonify({'status': sabnzbd['status']})
     except:
         pass
 
-    return jsonify ({'status': False})
+    return jsonify({'status': False})
+
 
 @app.route('/xhr/sabnzbd/individual/<state>/<id>/')
 @requires_auth
 def sabnzb_individual_toggle(state, id):
     try:
-        result = urllib.urlopen(sabnzbd_url('queue', '&name=%s&value=%s'%(state, id))).read()
+        result = urllib.urlopen(sabnzbd_url('queue', '&name=%s&value=%s' % (state, id))).read()
         sabnzbd = json.JSONDecoder().decode(result)
         if sabnzbd:
-            return jsonify ({'status': sabnzbd['status']})
+            return jsonify({'status': sabnzbd['status']})
     except:
         pass
 
-    return jsonify ({'status': False})
+    return jsonify({'status': False})
+
 
 @app.route('/sabnzbd/add/', methods=['POST'])
 def add_to_sab():
     try:
         url = request.form['url']
     except:
-        return jsonify({ 'error': 'Did not receive URL variable'})
+        return jsonify({'error': 'Did not receive URL variable'})
 
     try:
         return urllib.urlopen(url).read()
     except:
-        return jsonify({ 'error': 'Failed to open URL: %s' %(url)})
+        return jsonify({'error': 'Failed to open URL: %s' % (url)})
