3c7a46f78cd4b1f3a86e58e4944631f61d7aa592
Guido S. Nickels <gsn@kernel-oops.de>
2014-02-12 23:07:39 +0100

    fixed login bug and added verbosity
    - fixed typo causing password error
    - added connection verification
    - added some verbosity to the template (connection error, empty torrent list)
    - added debug logging for connection errors

--- modules/rtorrentdl.py
+++ modules/rtorrentdl.py
@@ -3,9 +3,12 @@
 
 from flask import Flask, jsonify, render_template
 from datetime import timedelta
-from Maraschino import app
+from maraschino import app, logger
 from maraschino.tools import *
 from rtorrent import RTorrent
+
+def log_error(ex):
+    logger.log('RTORRENTDL :: EXCEPTION - %s' % ex, 'DEBUG')
 
 @app.route('/xhr/rtorrentdl/')
 @requires_auth
@@ -15,57 +18,64 @@ def xhr_rtorrentdl():
 	# of active torrents
 	torrentlist = list()
 
+	# connection flag
+	connected = False
+
 	try:
-		if get_setting_value('rtorrent_user') and get_setting_value('rtorrent_password'):
+		if get_setting_value('rtorrent_url') is not None:
 			client = RTorrent(
 				get_setting_value('rtorrent_url'),
 				get_setting_value('rtorrent_user'),
-				get_setting_Value('rtorrent_password')
+				get_setting_value('rtorrent_password'),
+				True
 			)
-		else:
-			client = RTorrent(get_setting_value('rtorrent_url'))
 
-		# loop through each job, add any active (downloading) torrents to torrentlist()
-		# torrent: { info_hash: <HASH>, name: <NAME> }
-		for torrent in client.get_torrents():
-			# friendly status
-			if torrent.complete:
-				if torrent.active:
-					status = 'seeding'
+			if client is not None:
+				connected = True
+
+			# loop through each job, add any active (downloading) torrents to torrentlist()
+			# torrent: { info_hash: <HASH>, name: <NAME> }
+			for torrent in client.get_torrents():
+				# friendly status
+				if torrent.complete:
+					if torrent.active:
+						status = 'seeding'
+					else:
+						status = 'done'
 				else:
-					status = 'done'
-			else:
-				if torrent.active:
-					status = 'busy'
-				else:
-					status = 'inactive'
+					if torrent.active:
+						status = 'busy'
+					else:
+						status = 'inactive'
 
-			# get torrent file list
-			torrent_filelist = []
-			for file_current in torrent.get_files():
-				torrent_filelist.append(os.path.join(torrent.directory,file_current.path))
+				# get torrent file list
+				torrent_filelist = []
+				for file_current in torrent.get_files():
+					torrent_filelist.append(os.path.join(torrent.directory,file_current.path))
 
-			# what's left?
-			percent_done = float(100.0 / torrent.size_bytes * (torrent.size_bytes-torrent.left_bytes))
-			time_left = str(timedelta(seconds = float(torrent.left_bytes) / torrent.down_rate)) if torrent.down_rate > 0 else -1
+				# what's left?
+				percent_done = float(100.0 / torrent.size_bytes * (torrent.size_bytes-torrent.left_bytes))
+				time_left = str(timedelta(seconds = float(torrent.left_bytes) / torrent.down_rate)) if torrent.down_rate > 0 else -1
 
-			# append to list
-			torrentlist.append({
-				'name': torrent.name,
-				'id': torrent.info_hash,
-				'status': status,
-				'torrent_state': torrent.state,
-				'progress': percent_done,
-				'sec_left': time_left,
-				'seed_ratio': torrent.ratio,
-				'folder': torrent.directory,
-				'files': '|'.join(torrent_filelist)
-			})
+				# append to list
+				torrentlist.append({
+					'name': torrent.name,
+					'id': torrent.info_hash,
+					'status': status,
+					'torrent_state': torrent.state,
+					'progress': percent_done,
+					'sec_left': time_left,
+					'seed_ratio': torrent.ratio,
+					'folder': torrent.directory,
+					'files': '|'.join(torrent_filelist)
+				})
 
-		# no torrents -> empty list
-		if not torrentlist.__len__():
-			torrentlist = None
-	except:
+			# no torrents -> empty list
+			if not torrentlist.__len__():
+				torrentlist = None
+
+	except Exception as ex:
+		log_error(ex)
 		torrentlist = None
 
 	return render_template('rtorrentdl.html',
